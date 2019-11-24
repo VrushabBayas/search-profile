bfc8082ad31255d443fe03011e49cdf65d230d43
VrushabBayas <rushab.bayas@gmail.com>
2019-11-24 10:10:02 +0530

    Paginator ui integration

---
+++ src/Components/Paginator/Paginator.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import ReactPaginate from 'react-paginate';
+
+import './Styles.css';
+const Pagignator = ({ pageCount, handlePageClick }) => {
+	return (
+		<div className="paginator">
+			<ReactPaginate
+				previousLabel={'previous'}
+				nextLabel={'next'}
+				breakLabel={'...'}
+				breakClassName={'break-me'}
+				pageCount={pageCount}
+				marginPagesDisplayed={2}
+				pageRangeDisplayed={5}
+				onPageChange={handlePageClick}
+				containerClassName={'pagination'}
+				subContainerClassName={'pages pagination'}
+				activeClassName={'active'}
+			/>
+		</div>
+	);
+};
+
+Pagignator.propTypes = {};
+
+export default Pagignator;
